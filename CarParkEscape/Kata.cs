efe1836e732056a89564fe54ead922040af41bae
TorLindstrom <tor.lindstrom@winlas.se>
2024-11-11 13:31:35 +0100

    refactor: extract methods for getting distance to exit

--- CarParkEscape/Kata.cs
+++ CarParkEscape/Kata.cs
@@ -2,10 +2,30 @@ public class Kata
 {
     public static string[] escape(int[,] carpark)
     {
-        if (carpark.GetLength(1) == 0)
+        var floorLength = carpark.GetLength(1);
+        
+        if (floorLength == 0)
             return [];
-        if (carpark.GetLength(1) == 4)
-            return ["R3"];
-        return ["R1"];
+        
+        var startPosition = GetStartPosition(carpark);
+        
+        return ["R" + GetDistance(floorLength, startPosition)];
     }
- }
+
+    private static int GetDistance(int floorLength, int startPosition)
+    {
+        return floorLength - startPosition - 1;
+    }
+
+    private static int GetStartPosition(int[,] carpark)
+    {
+        var startPosition = -1;
+        for (var i = 0; i < carpark.GetLength(1); i++)
+        {
+            if (carpark[0, i] == 2)
+                startPosition = i;
+        }
+
+        return startPosition;
+    }
+}
